0a7ad27feff5a412fd2ceee76420772ae907bfae
Kanix <kanix@email.com>
2016-03-05 20:47:01 +0800

    Loaded csv and updated storyboard

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,32 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Kanix Lau on 5/3/2016.
+//  Copyright © 2016年 KanixLau. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
